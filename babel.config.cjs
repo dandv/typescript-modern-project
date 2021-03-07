536ce3b4bd7205b989aa9c0d66e5b14eb7da9b22
Dan Dascalescu <ddascalescu+github@gmail.com>
2021-03-06 19:50:55 -1000

    Delete leftover babel config file

--- babel.config.cjs
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  plugins: [
-    // Because Jest still doesn't support native ES modules - https://github.com/facebook/jest/issues/4842
-    '@babel/plugin-transform-modules-commonjs',
-  ],
-};
